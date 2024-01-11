8e4bd8e1d0966f2f10b8c72b1a29ddd4ae3fd6b0
Yatharth Mathur <yatharth@hackerearth.com>
2024-01-11 21:40:57 +0530

    fix: added user first name

--- user/insert.sql
+++ user/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO users (id, username, email) VALUES ('1234', 'yatharth_mathur', 'yatharth.mathur@gmail.com');
+INSERT INTO users (id, username, email, first_name) VALUES ('1234', 'yatharth_mathur', 'yatharth.mathur@gmail.com', "yatharth");
