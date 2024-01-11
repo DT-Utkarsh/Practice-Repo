0aad2847a8fd30521668799f381518718808dffe
DT-Utkarsh <utkarsh.gupta@decision-tree.com>
2024-01-11 21:30:17 +0530

    chore: create main

---
+++ main.sql
@@ -0,0 +1,12 @@
+CREATE VIEW UserProductView AS
+SELECT
+    Users.UserID,
+    Users.UserName,
+    Users.Email,
+    Products.ProductID,
+    Products.ProductName,
+    Products.Price
+FROM
+    Users
+JOIN Purchases ON Users.UserID = Purchases.UserID
+JOIN Products ON Purchases.ProductID = Products.ProductID;
